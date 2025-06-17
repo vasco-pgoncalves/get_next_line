44a399f0533dc64f95a723e0a8e81b4a16c1bc03
vasco-pgoncalves <vpinto-g@student.42lisboa.com>
2025-06-17 14:43:23 +0100

    Add files via upload

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,57 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpinto-g <vpinto-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:34:45 by vpinto-g          #+#    #+#             */
-/*   Updated: 2025/06/11 16:34:23 by vpinto-g         ###   ########.fr       */
+/*   Updated: 2025/06/11 16:44:45 by vpinto-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
-	static char		buf[BUFFER_SIZE + 1];
+	static char		buf[FOPEN_MAX][BUFFER_SIZE + 1];
 	int				counter;
 	char			*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd >= FOPEN_MAX || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = NULL;
-	if (*buf != '\0')
-		line = ft_strjoin (line, buf);
+	if (*buf[fd] != '\0')
+		line = ft_strjoin (line, buf[fd]);
 	while (ft_newline_counter(line) == -1)
 	{
-		counter = read (fd, buf, BUFFER_SIZE);
+		counter = read (fd, buf[fd], BUFFER_SIZE);
 		if (counter < 0)
 		{
-			buf[0] = '\0';
+			buf[fd][0] = '\0';
 			return (free(line), NULL);
 		}
 		if (counter == 0)
 			break ;
-		buf[counter] = '\0';
-		line = ft_strjoin(line, buf);
+		buf[fd][counter] = '\0';
+		line = ft_strjoin(line, buf[fd]);
 	}
 	line = ft_line_builder(line);
-	ft_clean_stash(buf);
+	ft_clean_stash(buf[fd]);
 	return (line);
 }
-/* int main(void)
+int main(void)
 {
-	int	fd = open("text.txt", O_RDONLY);
+	int	i;
+	int	fd1 = open("text1.txt", O_RDONLY);
+	int	fd2 = open("text2.txt", O_RDONLY);
+	int	fd3 = open("text3.txt", O_RDONLY);
 	char *line;
 
-	line = get_next_line(fd);
+	i = 3;
 
-	while (line)
+	while (i--)
 	{
-		printf("/%s", line);
+		line = get_next_line(fd1);
+		printf("%s", line);
 		free(line);
-		line = get_next_line(fd);
+		line = get_next_line(fd2);
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd3);
+		printf("%s", line);
+		free(line);
 	}
-	close(fd);
-} */
+	close(fd1);
+	close(fd2);
+	close(fd3);
+}
